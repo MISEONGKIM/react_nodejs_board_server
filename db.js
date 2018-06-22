ca6ab901e8f9bc4efa1777e1c397d155f786e87c
김 미성 <rlaaltjd46@jinhak.com>
2018-06-22 18:11:39 +0900

    0622

--- db.js
+++ db.js
@@ -1,6 +1,15 @@
 var sql = require("mssql");
 var config = require("./config/config.json");
 
-const pool = new sql.ConnectionPool({ config });
-
-pool.connect(err => {});
+const pool = new sql.ConnectionPool(config)
+  .connect()
+  .then(pool => {
+    return pool.query("select * from board");
+  })
+  .then(result => {
+    alert(result.recordset[0].value);
+    //console.dir(result.recordset[0].value);
+  })
+  .catch(err => {
+    console.log(err);
+  });
